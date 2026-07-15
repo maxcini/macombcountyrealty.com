bca8fb93ecc137cd06c5c769522ed2cec961e443
maxcini <123189582+maxcini@users.noreply.github.com>
2026-07-14 21:11:39 -0400

    remove auto backslash, make about page shorter, and add purgecss integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+import purgecss from 'astro-purgecss';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://macombcountyrealty.com',
-  integrations: [sitemap()],
-  trailingSlash: 'always',
+  integrations: [
+    sitemap(), 
+    purgecss()
+  ], 
 });
